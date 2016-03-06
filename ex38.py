ac76b061de300b2f8e630cc35db556bf39150fd2
Justin <jreans@gmail.com>
2016-03-06 02:12:21 -0500

    Fix typo in ex38

--- ex38.py
+++ ex38.py
@@ -2,7 +2,7 @@
 
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
-print "With there's not ten things in that list, let's fix that."
+print "Wait there are not ten things in that list, let's fix that."
 
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
